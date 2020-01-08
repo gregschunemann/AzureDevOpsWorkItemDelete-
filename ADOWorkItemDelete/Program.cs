6974e330c698354d0d2e078fb698469c940ba8d1
Greg Schunemann <grschune@microsoft.com>
2020-01-08 16:03:41 -0500

    initial commit

---
+++ ADOWorkItemDelete/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ADOWorkItemDelete
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            const string URL = "<YourOrgURL>";  //example - https://dev.azure.com/myOrg
+            const string PAT = "YourPAT"; //Personal Access Token with work items read/write access
+            const string projectName = "YourProjectName";
+            string val;
+            int workItemStart;
+            int workItemEnd;
+
+            Console.Write("Enter the first work item number in the range: ");
+            val = Console.ReadLine();
+            workItemStart = Convert.ToInt32(val);
+
+            Console.Write("Enter the last work item number in the range: ");
+            val = Console.ReadLine();
+            workItemEnd = Convert.ToInt32(val);
+
+
+            for (int i = workItemStart; i <= workItemEnd; i++) 
+            {
+                try
+                {
+                    var responseMessage = WorkItemProcessor.Delete(i, URL, projectName, PAT).Result;
+                    //Console.WriteLine("Response code: " + responseMessage.StatusCode);
+                    Console.WriteLine("Work item successfully deleted.");
+                }
+                catch (AggregateException e)
+                {
+                    //Console.WriteLine($"Work item not found: {e}");
+                    Console.WriteLine($"Work item not found: {i}");
+                }
+               
+                //Console.WriteLine("Response code: " + responseMessage.StatusCode);
+            }
+        }
+    }
+}
---
+++ ADOWorkItemDelete/WorkItemProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace ADOWorkItemDelete
+{
+    public class WorkItemProcessor
+    {
+        public static async Task<HttpResponseMessage> Delete(int id, string projectUri, string projectName, string personalAccessToken)
+        {
+            //DELETE https://dev.azure.com/{organization}/{project}/_apis/wit/workitems/{id}?api-version=5.1
+
+            var deleteUri = Url.Combine(projectUri, projectName, "/_apis/wit/workitems/", id.ToString(), "?destroy=true&api-version=5.1");
+
+            var responseMessage = await deleteUri.WithBasicAuth(string.Empty, personalAccessToken).DeleteAsync();
+
+            return responseMessage;
+        }
+    }
+}
